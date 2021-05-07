3b4e0d8d0f02682170be9a54b10a40a56735247e
Nitin Bisht <3498georgian@gmail.com>
2021-05-07 16:30:35 +0530

    Initial Commit

---
+++ spring-security-jdbc/src/main/java/com/bisht/springsecurityjdbc/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.bisht.springsecurityjdbc;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        
+    }
+}
---
+++ spring-security-jdbc/src/main/java/com/bisht/springsecurityjdbc/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.bisht.springsecurityjdbc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "<h1>Welcome</h1>";
+    }
+
+    @RequestMapping("/user")
+    public String user() {
+        return "<h1>Welcome User</h1>";
+    }
+
+
+    @RequestMapping("/admin")
+    public String admin() {
+        return "<h1>Welcome Admin</h1>";
+    }
+}
